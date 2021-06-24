45a508cda5c7f73f9460d7bc462fe0e7905fd02a
Marcelo Luiz <mlluizpereira39@gmail.com>
2021-06-23 22:57:40 -0300

    finalizado o css e iniciado o js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-const form = document.querySelector('form')
+const inputElement = document.querySelector('.inputField input')
+const buttonElement = document.querySelector('.inputField button')
+const ulElement = document.querySelector('[data-js="todoList"]')
+const footerElement = document.querySelector('[data-js="footer"]')
+
+  buttonElement.addEventListener('click', () => {
+    if(inputElement.value) {
+
+      const liElement = document.createElement('li')
+      liElement.innerHTML = `<li>${inputElement.value}<span><i class="fas fa-trash"></i></span></li>`
+      ulElement.prepend(liElement)
+      
+      liElement.addEventListener('click', event => {
+      const clickedElement = event.currentTarget
+
+      clickedElement.remove()
+      })
+      footerElement.textContent = `você tem  tarefas pendentes`  
+    }
+    inputElement.value = ''
+  })
+
+  
+
+  
+
+  
+
+
+
