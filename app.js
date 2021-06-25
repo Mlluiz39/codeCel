af4a8c374c6e5e9ebdc662db7511385bdd7d54a8
Marcelo Luiz <mlluizpereira39@gmail.com>
2021-06-24 22:15:54 -0300

    finalizado o javascript

--- app.js
+++ app.js
@@ -1,30 +1,75 @@
-const inputElement = document.querySelector('.inputField input')
-const buttonElement = document.querySelector('.inputField button')
-const ulElement = document.querySelector('[data-js="todoList"]')
-const footerElement = document.querySelector('[data-js="footer"]')
+const inputElement = document.querySelector('[data-js="input"]');
+const buttonElement = document.querySelector('[data-js="button"]');
+const todoListElement = document.querySelector('[data-js="todoList"]');
+const footerElement = document.querySelector('[data-js="footer"]');
+const deleteAllTasks = document.querySelector('[data-js="deleteTasks"]')
 
-  buttonElement.addEventListener('click', () => {
-    if(inputElement.value) {
+inputElement.addEventListener('keyup', () => {
+  const userData = inputElement.value;
+  if (userData) {
+    buttonElement.classList.add('active');
+  } else {
+    buttonElement.classList.remove('active');
+  }
+});
+showTasks();
 
-      const liElement = document.createElement('li')
-      liElement.innerHTML = `<li>${inputElement.value}<span><i class="fas fa-trash"></i></span></li>`
-      ulElement.prepend(liElement)
-      
-      liElement.addEventListener('click', event => {
-      const clickedElement = event.currentTarget
+buttonElement.addEventListener('click', () => {
+  let userData = inputElement.value;
 
-      clickedElement.remove()
-      })
-      footerElement.textContent = `você tem  tarefas pendentes`  
+  let getLocalStorage = localStorage.getItem('New Todo');
+  if (getLocalStorage === null) {
+    listArray = [];
+  } else {
+    listArray = JSON.parse(getLocalStorage);
+  }
+  listArray.push(userData);
+  localStorage.setItem('New Todo', JSON.stringify(listArray));
+  showTasks();
+});
+
+function showTasks() {
+  let getLocalStorage = localStorage.getItem('New Todo');
+  if (getLocalStorage === null) {
+    listArray = [];
+  } else {
+    listArray = JSON.parse(getLocalStorage);
+  }
+
+  if (listArray.length <= 0) {
+    footerElement.textContent = `Você  naõ tem tarefas pendente`;
+  }else if (listArray.length === 1) {
+    footerElement.textContent = `Você tem ${listArray.length} tarefa pendente`;
+  } else {
+    footerElement.textContent = `Você tem ${listArray.length} tarefas pendentes`;
+
+    if (listArray.length > 0) {
+      deleteAllTasks.classList.add('active')
+    }else {
+      deleteAllTasks.classList.remove('active')
     }
-    inputElement.value = ''
-  })
+  }
 
-  
+  let newLiElement = '';
+  listArray.forEach((element, index) => {
+    newLiElement += `<li>${element}<span onclick="deleteTasks(${index})"><i class="fas fa-trash"></i></span></li>`;
+  });
 
-  
+  todoListElement.innerHTML = newLiElement;
+  inputElement.value = '';
+  buttonElement.classList.remove('active');
+}
 
-  
+function deleteTasks(index) {
+  let getLocalStorage = localStorage.getItem('New Todo');
+  listArray = JSON.parse(getLocalStorage);
+  listArray.splice(index, 1);
+  localStorage.setItem('New Todo', JSON.stringify(listArray));
+  showTasks();
+}
 
-
-
+deleteAllTasks.addEventListener('click', () => {
+  listArray = []
+  localStorage.setItem('New Todo', JSON.stringify(listArray));
+  showTasks();
+})
